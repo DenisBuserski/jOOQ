44400270915d2abd0bc6e726c70bcb1cf3375ae5
Denis Buserski <denis.buserski@gmail.com>
2023-09-08 23:14:01 +0300

    Update setup.sql

--- java-jooq/src/main/resources/setup.sql
+++ java-jooq/src/main/resources/setup.sql
@@ -3,8 +3,8 @@ CREATE DATABASE `java_jooq`;
 USE `java_jooq`;
 
 CREATE TABLE `users` (
- `id` int NOT NULL,
- `name` varchar(255),
- `age` int,
- PRIMARY KEY (`id`)
+`id` int NOT NULL,
+`name` varchar(255),
+`age` int,
+PRIMARY KEY (`id`)
 );
